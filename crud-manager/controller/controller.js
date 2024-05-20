1054ee9c8f6ef9b339cdd0792ba9be28e686fafe
Muhammad Vidi <vidim@impstudio.co.id>
2024-05-20 09:41:48 +0700

    first commit

--- crud-manager/controller/controller.js
+++ crud-manager/controller/controller.js
@@ -1,18 +1,16 @@
-
-// const { publish, publisher } = require('../config/redis')
 const Redis = require('redis')
 
 async function create(req, res) {
   const publisher = Redis.createClient(process.env.REDIS_PORT, process.env.REDIS_HOST)
   const authHeader = req.get('authorization')
   const token = authHeader.split(' ')[1]
-  // console.log(token)
+  
   const authModuleUrl = `${process.env.AUTH_MODULE_URL}/api/v1/auth/validate?accessToken=${token}`
 
-  // console.log(authModuleUrl)
+  
   try {
     const validate = await fetch(authModuleUrl)
-    // console.log(validate)
+    
     if (validate.status !== 200) {
       return res.status(400).send({
         message: 'Not Verified'
@@ -22,7 +20,7 @@ async function create(req, res) {
     console.log(data)
     await publisher.connect()
     await publisher.publish(process.env.PUBSUB_CHANNEL, data)
-    // console.log('publish > ', publishData)
+    
     console.log(`Publishing event..`)
     return res.send({message: 'Publishing event successful'})
 
